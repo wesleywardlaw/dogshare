cb79b4362a28a4456f0bde7e2e7d60ca95c02133
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2017-09-30 12:10:47 -0500

    edit mongoose connect options

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes = require("./routes/comments"),
 
 
 // mongoose.connect("mongodb://localhost/dogshare");
-mongoose.connect("mongodb://wesuser:Snfrgh03!@ds157614.mlab.com:57614/dogshare");
+mongoose.connect("mongodb://wesuser:Snfrgh03!@ds157614.mlab.com:57614/dogshare", { useMongoClient: true, promiseLibrary: global.Promise });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
